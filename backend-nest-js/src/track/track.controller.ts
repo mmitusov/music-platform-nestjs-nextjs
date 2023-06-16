72821c93a8703bffb91e64bbceb77d6503b85962
mmitusov <maksym.mitusov@gmail.com>
2023-06-16 15:52:04 +0300

    Track controller implementation

--- backend-nest-js/src/track/track.controller.ts
+++ backend-nest-js/src/track/track.controller.ts
@@ -1,23 +1,25 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { TrackService } from "./track.service";
 import { CreateTrackDto } from "./dto/create-track.dto";
+import { ObjectId } from "mongoose";
 
 @Controller('/tracks')
 export class TrackController {
     constructor(private trackService: TrackService) {}
 
     @Post()
-    create(@Body() createTrackDto: CreateTrackDto) {
+    create(@Body() createTrackDto: CreateTrackDto) { //CreateTrackDto - type for TypeScript
         return this.trackService.create(createTrackDto)
     }
 
     @Get()
     getAll() {
-        return 'Test track message'
+        return this.trackService.getAll()
     }
 
-    getOne() {
-
+    @Get(':trackId')
+    getOne(@Param('trackId') id: ObjectId) { //ObjectId - type for TypeScript
+        return this.trackService.getOne(id)
     }
 
     delete() {
--- backend-nest-js/src/track/track.service.ts
+++ backend-nest-js/src/track/track.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Model, ObjectId } from "mongoose";
 import { Track } from "./schemas/track.schemas";
 import { Comment } from "./schemas/comment.schemas";
 import { CreateTrackDto } from "./dto/create-track.dto";
@@ -12,17 +12,19 @@ export class TrackService {
         @InjectModel(Comment.name) private commentModel: Model<Comment>,
     ) {}
 
-    async create(createTrackDto: CreateTrackDto): Promise<Track> {
-        const createdTrack = await this.trackModel.create({createTrackDto, listened: 0});
+    async create(createTrackDto: CreateTrackDto): Promise<Track> { //CreateTrackDto - type for TypeScript
+        const createdTrack = await this.trackModel.create({...createTrackDto, listened: 0});
         return createdTrack;
     }
 
-    async getAll() {
-
+    async getAll(): Promise<Track[]> {
+        const getAllTrack = await this.trackModel.find();
+        return getAllTrack;
     }
 
-    async getOne() {
-
+    async getOne(id: ObjectId): Promise<Track> { //ObjectId - type for TypeScript
+        const getOneTrack = await this.trackModel.findById(id);
+        return getOneTrack;
     }
 
     async delete() {
