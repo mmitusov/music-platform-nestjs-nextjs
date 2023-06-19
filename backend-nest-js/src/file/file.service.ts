0ea062684b91b2ae2a140220d06e491e5e817e55
mmitusov <maksym.mitusov@gmail.com>
2023-06-19 15:00:55 +0300

    Description update

--- backend-nest-js/src/file/file.service.ts
+++ backend-nest-js/src/file/file.service.ts
@@ -1,5 +1,13 @@
-//Чтобы сохрнить файл, сперва нам нужно сгенерировать путь к этому файлу
-//
+//Чтобы сохрнить файл, сперва нам нужно сгенерировать путь где он будет храниться: const filePath = ...
+//Далее нам нужно задать уникальное имя для файла (что-то типо его id), чтобы мы могли его идентифицировать: const fileName = ...
+//Так как расширение файла может быть разным, выцепим его из того параметра, что мы принемаем в функцию: const fileExtention =  ...
+//И чтобы сгенерированое имя нашего файла имело расширение, добавим его к имени файла: uuid.v4() + fileExtention;
+
+//Затем мы хотим проверить, существует ли вообще папка по тому пути, куда мы хотим сохранить файл
+//Еслии нет, то мы ее создадим: fs.mkdirSync(filePath, {recursive: true})
+//{recursive: true}: Если несколько вложенных папок не существует, то они все по очереди будут создаваться
+
+//После сохранения файла, возвращает строку с его типом и именем
 
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import * as path from "path"; //Модули из NodeJS для работы с путями. Чтобы они были одинаковы для всех OS
@@ -15,13 +23,13 @@ export enum FileType {
 export class FileService {
     createFile(type: FileType, file): string {
         try { //Так как при записи файлов могут возникнуть ошибки - завернем все в try/catch
-            const fileExtention = file.originalname.split('.').pop();
+            const fileExtention = file.originalname.split('.').pop(); //.originalname - это метод из встроемной библиотеки 'multer'
             const fileName = uuid.v4() + fileExtention;
             const filePath = path.resolve(__dirname, '..', 'static', fileName);
             if (!fs.existsSync(filePath)) {
                 fs.mkdirSync(filePath, {recursive: true})
             };
-            fs.writeFileSync(path.resolve(filePath, fileName), file.buffer);
+            fs.writeFileSync(path.resolve(filePath, fileName), file.buffer); //Склеиваем путь к папке и название файла, после чего сохраняем файл; ".buffer" - это метод из 'multer'
             return type + '/' + fileName;
         } catch(e) {
             throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR)
