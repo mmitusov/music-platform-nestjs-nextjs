9f7824c557fb3cc13b31863e05d85e3c4514ed76
mmitusov <maksym.mitusov@gmail.com>
2023-06-23 11:14:09 +0300

    Frontend - Code refactoring

--- frontend-next-js/src/components/burger/burgerMain.tsx
+++ frontend-next-js/src/components/burger/burgerMain.tsx
@@ -1,17 +1,20 @@
 import burgerMainStyles from '@/styles/burger/burgerMain.module.scss'
+import Link from 'next/link';
 
 const BurgerMain = () => {
     return (
-        <div className={`${burgerMainStyles.mainContainer}`}>
+        <>
             <label className={`${burgerMainStyles.hamburgerBtn}`}>
                 <span />
                 <input type='checkbox'/>
                 <span />
             </label>
             <div className={`${burgerMainStyles.sideMenu}`}>
-                
+                <h1>Main</h1>
+                <h1><Link href="/tracks">Tracks</Link></h1>
+                <h1>Albums</h1>
             </div>
-        </div>
+        </>
     );
 }
  
