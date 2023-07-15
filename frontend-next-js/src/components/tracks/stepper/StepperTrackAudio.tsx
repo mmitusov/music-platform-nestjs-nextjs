f4df98b1cbe26f38afc7494f41e8b289cb6fe775
mmitusov <maksym.mitusov@gmail.com>
2023-07-15 20:52:21 +0300

    Frontend - Stepper update

--- frontend-next-js/src/components/tracks/stepper/StepperTrackAudio.tsx
+++ frontend-next-js/src/components/tracks/stepper/StepperTrackAudio.tsx
@@ -3,26 +3,30 @@ import { useRef, useState } from 'react'
 
 const StepperTrackAudio = ({activeStep, setActiveStep, setOverallInput, sendNewTrack}) => {
     const [audioInput, setAudioInput] = useState(null)
+    const [audioName, setAudioName] = useState('')
     const audioRef = useRef<HTMLInputElement>()
 
     const onChange = (e) => {
+        setAudioName(e.target.files[0].name)
         setAudioInput(e.target.files?.[0])
         setOverallInput( prev => ({ ...prev, [ e.target.id ]: e.target.files?.[0]}) );
     }
     
     return (
         <div className={`${trackAudioStyes.trackAudioContainer}`}>
-            <button onClick={() => audioRef.current?.click()}> {/* При помощи onClick - мы нажимаем на спрятаный инпут*/}
-                <input 
-                    id="trackAudio"
-                    type='file' 
-                    accept={'audio/*'} 
-                    ref={audioRef} 
-                    onChange={onChange}
-                    hidden
-                />
-                Upload audio
-            </button>
+            <div className={`${trackAudioStyes.uploadAudio}`}>
+                <button onClick={() => audioRef.current?.click()}> {/* При помощи onClick - мы нажимаем на спрятаный инпут*/}
+                    <input 
+                        id="trackAudio"
+                        type='file' 
+                        accept={'audio/*'} 
+                        ref={audioRef} 
+                        onChange={onChange}
+                        hidden
+                    />
+                    {audioName || 'Upload audio'}
+                </button>
+            </div>
             <div className={`${trackAudioStyes.navigationButtons}`}>
                 <button disabled={activeStep === 0} onClick={() => setActiveStep(activeStep-1)}>Previous step</button>
                 <button disabled={!audioInput} onClick={() => sendNewTrack()}>Create track</button>
--- frontend-next-js/src/components/tracks/stepper/StepperTrackPicture.tsx
+++ frontend-next-js/src/components/tracks/stepper/StepperTrackPicture.tsx
@@ -1,28 +1,43 @@
 import trackPictureStyes from '@/styles/components/stepper/stepperTrackPicture.module.scss'
+import Image from 'next/image';
 import { useRef, useState } from 'react';
 
 const StepperTrackPicture = ({activeStep, setActiveStep, setOverallInput}) => {
     const [pictureInput, setPictureInput] = useState(null)
+    const [pictureName, setPictureName] = useState('')
+    const [displayPicture, setDisplayPicture] = useState<any>(null)
     const imgRef = useRef<HTMLInputElement>()
 
     const onChange = (e) => {
+        setPictureName(e.target.files[0].name)
         setPictureInput(e.target.files?.[0])
+        setDisplayPicture(URL.createObjectURL(e.target.files[0]))
         setOverallInput( prev => ({ ...prev, [ e.target.id ]: e.target.files?.[0]}) );
     }
-
+    
     return (
         <div className={`${trackPictureStyes.trackPictureContainer}`}>
-            <button onClick={() => imgRef.current?.click()}> {/* При помощи onClick - мы нажимаем на спрятаный инпут*/}
-                <input 
-                    id="trackPicture"
-                    type='file' 
-                    accept={'image/*'} 
-                    ref={imgRef} 
-                    onChange={onChange}
-                    hidden
-                />
-                Upload image
-            </button>
+            <div className={`${trackPictureStyes.uploadPicture}`}>
+                <span>
+                    <Image 
+                        src={ displayPicture || '' } 
+                        alt=''
+                        fill
+                    /> 
+                </span>
+                <button onClick={() => imgRef.current?.click()}> {/* При помощи onClick - мы нажимаем на спрятаный инпут*/}
+                    <input 
+                        id="trackPicture"
+                        type='file' 
+                        accept={'image/*'} 
+                        ref={imgRef} 
+                        onChange={onChange}
+                        hidden
+                    />
+                    {pictureName || 'Upload image'}
+                </button>
+            </div>
+
             <div className={`${trackPictureStyes.navigationButtons}`}>
                 <button disabled={activeStep === 0} onClick={() => setActiveStep(activeStep-1)}>Previous step</button>
                 <button disabled={activeStep >= 2 || !pictureInput} onClick={() => setActiveStep(activeStep+1)}>Next step</button>
